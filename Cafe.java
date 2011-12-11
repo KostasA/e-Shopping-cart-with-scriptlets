66983b56f2a1e8946297d6685cf3bcfcbeb591c0
Konstantinos Anastasiou <turbokotsios@hotmail.com>
2011-12-11 18:42:09 +0200

    first commit

---
+++ Cafe.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foo;
+
+/**
+ *
+ * @author konanast
+ */
+public class Cafe {
+    
+    double cafequa;
+    
+    public void setCafequa(double cafequa) {
+        this.cafequa=cafequa;
+    }
+    
+    public double getCafequa() {
+        return cafequa;
+    }
+}
