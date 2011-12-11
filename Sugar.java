59ff2ed80eed759955d59c74a08dfe23771f86f6
Konstantinos Anastasiou <turbokotsios@hotmail.com>
2011-12-11 18:42:36 +0200

    first commit

---
+++ Sugar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foo;
+
+/**
+ *
+ * @author konanast
+ */
+public class Sugar {
+    double sugarqua;
+    
+    public void setSugarqua(double sugarqua) {
+        this.sugarqua=sugarqua;
+    }
+    
+    public double getSugarqua() {
+        return sugarqua;
+    }
+}
