fe814c4ccb88c8e07b23c3030801d775626470fc
Konstantinos Anastasiou <turbokotsios@hotmail.com>
2011-12-11 18:42:56 +0200

    first commit

---
+++ Water.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foo;
+
+/**
+ *
+ * @author konanast
+ */
+public class Water {
+    double waterqua;
+    
+    public void setWaterqua(double waterqua) {
+        this.waterqua=waterqua;
+    }
+    
+    public double getWaterqua() {
+        return waterqua;
+    }
+}
