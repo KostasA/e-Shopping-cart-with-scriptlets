6697afbce2b2d40f77472ace10f6c9d19326757a
KostasA <konanast@gmail.com>
2015-05-20 10:32:17 +0200

    Update Cafe.java
    This commit was made to clarify the use of this keyword and the main 
    use of the constructor.
    There is no matter here.

--- Cafe.java
+++ Cafe.java
@@ -10,10 +10,24 @@ package foo;
  */
 public class Cafe {
     
+    /****************************instance variable******************************/
     double cafequa;
+    /***************************************************************************/
+    
+    /************************creating a constructor****************************/
+    //every class has a constructor. The main rule of constructors is that they
+    //should have the same name as the class.
+    
+    //public Cafe(double cafequa){
+    //   this.cafequa = cafequa;   
+    //}
+    
     
     public void setCafequa(double cafequa) {
-        this.cafequa=cafequa;
+        //The constructor's argument shadows the object's field.
+        //inside the constructor cafequa is a local copy of the constructor's first argument
+        //To refer to the Cafe field cafequa, the constructor must use this.cafequa
+        this.cafequa=cafequa; //the most common reason for using the this keyqord is because a field is shadowed by a method or constructor parameter.
     }
     
     public double getCafequa() {
